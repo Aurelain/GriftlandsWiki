59886cb0b84ed501519427a27c4c0e6f4d185605
Aurelain <aurelain@gmail.com>
2021-07-28 16:25:50 +0200

    proper card parsing

--- scripts/update/cardHelpers/parseCardContent.js
+++ scripts/update/cardHelpers/parseCardContent.js
@@ -1,9 +1,19 @@
 const assert = require('assert');
 const deshell = require('../../utils/deshell');
+const findEnclosure = require('../../utils/findEnclosure');
+const obfuscateLuaTexts = require('../../utils/obfuscateLuaTexts');
+const restoreLuaTexts = require('../../utils/restoreLuaTexts');
 
 // =====================================================================================================================
 //  D E C L A R A T I O N S
 // =====================================================================================================================
+const CONSTANTS = [
+    'TARGET_ANY_RESOLVE',
+    'EVENT_PRIORITY_MULTIPLIER',
+    'EVENT_PRIORITY_ADDITIVE',
+    'table.empty',
+    'ALL_DECKS',
+];
 
 // =====================================================================================================================
 //  P U B L I C
@@ -12,13 +22,85 @@ const deshell = require('../../utils/deshell');
  *
  */
 const parseCardContent = (enclosure, luaPath, id) => {
-    const content = deshell(enclosure);
+    let draft = deshell(enclosure);
+
+    draft = convertConstants(draft, id);
+
+    draft = obfuscateLuaTexts(draft);
+
+    draft = draft.split('=').join(':');
+    draft = removeFunctions(draft);
+    draft = adaptArrays(draft);
+    draft = adaptFlags(draft);
+
+    draft = restoreLuaTexts(draft);
+
+    const code = '({' + draft + '})';
+    let card;
+    try {
+        card = eval(code);
+        console.log('card:', card);
+    } catch (e) {
+        assert(false, `Card could not be parsed! ${e.message}\n${code}`);
+    }
     return {};
 };
 
 // =====================================================================================================================
 //  P R I V A T E
 // =====================================================================================================================
+/**
+ *
+ */
+const removeFunctions = (cardContent) => {
+    let draft = cardContent;
+    const functionsFound = cardContent.matchAll(/:\s*function/g);
+    for (const {index} of functionsFound) {
+        const functionBlock = findEnclosure(cardContent, index, '{', '}');
+        // TODO: salvage some information
+        draft = draft.split(functionBlock).join(': "function"');
+    }
+    return draft;
+};
+/**
+ *
+ */
+const adaptArrays = (cardContent) => {
+    let draft = cardContent;
+    const arraysFound = draft.matchAll(/:\s*{/g);
+    for (const {index} of arraysFound) {
+        const arrayBlock = findEnclosure(cardContent, index, '{', '}');
+        // TODO: salvage some information
+        draft = draft.split(arrayBlock).join(': "object"');
+    }
+    return draft;
+};
+
+/**
+ *
+ */
+const adaptFlags = (cardContent) => {
+    return cardContent.replace(/(\bflags\s*:)([^,}]*)/g, flagsReplacer);
+};
+
+/**
+ *
+ */
+const flagsReplacer = (line, flagsProp, flagsValue) => {
+    flagsValue = flagsValue.split('|').join(',');
+    flagsValue = flagsValue.split('negotiation_defs.').join('');
+    return flagsProp + '[' + flagsValue + ']';
+};
+
+/**
+ *
+ */
+const convertConstants = (cardContent) => {
+    for (const name of CONSTANTS) {
+        cardContent = cardContent.split(name).join(`"${name}"`);
+    }
+    return cardContent.replace(/([A-Z_0-9]+\.[A-Z_0-9]+)/g, '"$1"');
+};
 
 // =====================================================================================================================
 //  E X P O R T
--- scripts/utils/convertLuaToJs.js
+++ scripts/utils/convertLuaToJs.js
@@ -1,23 +1,23 @@
-const toBase64 = require('./toBase64');
-const fromBase64 = require('./fromBase64');
+const obfuscateLuaTexts = require('./obfuscateLuaTexts');
+const restoreLuaTexts = require('./restoreLuaTexts');
 
 /**
  *
  */
 const convertLuaToJs = (lua) => {
-    lua = lua.replace(/(")([^"]*)(")/g, hideTexts);
-    lua = lua.replace(/(\[\[)([\s\S]*?)(]])/g, hideTexts); // ai_negotiation quips
+    lua = lua.replace(/\[\[([\s\S]*?)]]/g, multilineReplacer); // "ai_negotiation" quips
+
+    lua = obfuscateLuaTexts(lua);
 
     lua = lua.replace(/\bfunction\b([^)]*)\)/g, 'function$1){');
     lua = lua.replace(/\bfor\b([\s\S]*?)\bdo\b/g, 'for ($1) {');
     lua = lua.replace(/\bwhile\b([\s\S]*?)\bdo\b/g, 'while ($1) {');
     lua = lua.replace(/\bif\b([\s\S]*?)\bthen\b/g, 'if ($1) {');
-    lua = lua.replace(/\belseif\b([\s\S]*?)\bthen\b/g, '} else if ($1) {');
     lua = lua.replace(/\belse\b/g, '} else {');
+    lua = lua.replace(/\belseif\b([\s\S]*?)\bthen\b/g, '} else if ($1) {');
     lua = lua.replace(/\bend\b/g, '}');
 
-    lua = lua.replace(/(")([^"]*)(")/g, restoreTexts);
-    lua = lua.replace(/(\[\[)([\s\S]*?)(]])/g, restoreTexts);
+    lua = restoreLuaTexts(lua);
 
     return lua;
 };
@@ -28,15 +28,8 @@ const convertLuaToJs = (lua) => {
 /**
  *
  */
-const hideTexts = (found, prefix, inner, suffix) => {
-    return prefix + toBase64(inner) + suffix;
-};
-
-/**
- *
- */
-const restoreTexts = (found, prefix, inner, suffix) => {
-    return prefix + fromBase64(inner) + suffix;
+const multilineReplacer = (found, inside) => {
+    return '"' + inside.replace(/[\r\n]+/g, ' ') + '"';
 };
 
 // =====================================================================================================================
