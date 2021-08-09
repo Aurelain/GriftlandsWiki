961382c0718d8b69e7e13ca23574051c5745859b
Aurelain <aurelain@gmail.com>
2021-08-09 23:18:31 +0300

    preparing character updates

--- scripts/update/update.js
+++ scripts/update/update.js
@@ -27,31 +27,27 @@ const {STORAGE, GAME_DIR, DEBUG} = require('../utils/CONFIG');
  */
 const update = async () => {
     try {
-        const assetsZip = new AdmZip(GAME_DIR + '/data.zip', {});
+        // const assetsZip = new AdmZip(GAME_DIR + '/data.zip', {});
         const scriptsZip = new AdmZip(GAME_DIR + '/data_scripts.zip', {});
 
-        const artIds = getArtIds(assetsZip);
+        // const artIds = getArtIds(assetsZip);
+        //
+        // const keywords = getKeywords(scriptsZip);
+        // await writeKeywordsSheet(keywords);
+        //
+        // const cards = getCards(scriptsZip, keywords, artIds);
+        // await writeCardsSheet(cards);
 
-        const keywords = getKeywords(scriptsZip);
-        await writeKeywordsSheet(keywords);
-
-        const cards = getCards(scriptsZip, keywords, artIds);
-        await writeCardsSheet(cards);
-
+        // Uncomment the following line to mass-import assets extracted by the CardExporter mod:
         // importCardPics(cards);
 
-        updateCards(cards);
+        // updateCards(cards);
 
-        return;
-
-        const characters = await getCharacters(zip);
-        const factions = getFactions(zip);
-        const prepared = prepareCharacters(characters, factions);
-        const charactersStatus = inspectCharacters(prepared);
-        if (!(await guard(charactersStatus, true))) {
-            return;
-        }
+        const characters = await getCharacters(scriptsZip);
         await writeCharactersSheet(characters);
+
+        const factions = getFactions(scriptsZip);
+        const prepared = prepareCharacters(characters, factions);
         writeCharacters(prepared);
     } catch (e) {
         console.log('Error:', e.message);
